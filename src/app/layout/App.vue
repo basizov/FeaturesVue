aa4b017b1c00a8498f25c7360e4d60a3624620e6
boris <borang.3006@yandex.ru>
2021-07-16 21:02:35 +0300

    Init commit
    Создана базовая структура проекта.

---
+++ src/app/layout/App.vue
@@ -0,0 +1,22 @@
+<template>
+  App
+</template>
+
+<script lang='ts'>
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: 'App',
+    setup() {
+
+
+      return {
+
+      }
+    }
+  });
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './app/layout/App.vue'
+import './app/layout/style.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+const vueApplication = createApp(App);
+
+vueApplication
+  .mount('#app');
